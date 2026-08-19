eee1dc6bbaf331e3dd0245b0364245bc6cce4a68
sophie-24 <gyuri5578@naver.com>
2026-08-19 15:44:37 +0900

    chore: 기존 Application 도메인 파일 정리

--- app/src/main/java/com/asbg/outboxlab/application/dto/ApplicationResult.java
+++
@@ -1,5 +0,0 @@
-package com.asbg.outboxlab.application.dto;
-
-import java.util.UUID;
-
-public record ApplicationResult(UUID applicationId, UUID eventId, String correlationId) {}
--- app/src/main/java/com/asbg/outboxlab/application/exception/DuplicateRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.asbg.outboxlab.application.exception;
-
-public class DuplicateRequestException extends RuntimeException {
-    public DuplicateRequestException(String idempotencyKey) {
-        super("이미 처리된 요청입니다: idempotencyKey=" + idempotencyKey);
-    }
-}
